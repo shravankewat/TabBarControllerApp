e01382d59382fa9cde5df22c2c4cb02477ee08d5
Amit Sharma <amitsh4022@gmail.com>
2023-03-23 19:23:59 +0530

    Initial Commit

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by Amit Sharma on 23/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
