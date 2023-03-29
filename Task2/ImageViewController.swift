5316d3e14e3fdd64ac5d59d6e544728377d8614a
Amit Sharma <amitsh4022@gmail.com>
2023-03-29 19:04:20 +0530

    complete department data and employee data

--- Task2/ImageViewController.swift
+++ Task2/ImageViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import SDWebImage
+
 struct AlbumResponse: Codable {
     let resultCount: Int
     let results: [Album]
@@ -23,42 +25,61 @@ class ImageViewController: UIViewController, UITableViewDelegate,UITableViewData
     var albums = [Album]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
         getData()
-//         Do any additional setup after loading the view.
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return albums.count
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = myTable.dequeueReusableCell(withIdentifier: "ArtistCell", for: indexPath) as! NewFileTableViewCell
         cell.artistNameLable.text = albums[indexPath.row].artistName
         cell.albumLable.text = albums[indexPath.row].collectionName
+        
+        let imageStr = albums[indexPath.row].artworkUrl100
+        if let imageurl = URL(string: imageStr){
+//            let data = try? Data(contentsOf: imageurl)
+//            cell.imageLable.image = UIImage(data: data!)
+            cell.imageLable.contentMode = .scaleAspectFill
+            cell.imageLable.sd_setImage(with: imageurl)
+            
+            
+            
+            
+        }else{
+            debugPrint("imagenotfound")
+        }
         return cell
     }
     
     func getData(){
-        let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson&limit=25")
-         URLSession.shared.dataTask(with: url!, completionHandler: {[weak self] data, response, error in
-             if let error = error {
-                 debugPrint(error)
-             }
-             else if let data = data {
-                 do {
-                     let albumResponse = try JSONDecoder().decode(AlbumResponse.self, from: data)
-                     self?.albums = albumResponse.results
-                     DispatchQueue.main.async {
-                         self?.myTable.reloadData()
-                     }
-                 } catch  {
-                     debugPrint(error)
-                 }
-             }
-             else {
-                 debugPrint("Something went wrong.")
-             }
+        let url = URL(string: "https://itunes.apple.com/search?term=jack+johnson&limit=60")
+        URLSession.shared.dataTask(with: url!, completionHandler: {[weak self] data, response, error in
+            if let error = error {
+                debugPrint(error)
+            }
+            else if let data = data {
+                do {
+                    let albumResponse = try JSONDecoder().decode(AlbumResponse.self, from: data)
+                    self?.albums = albumResponse.results
+                    DispatchQueue.main.async {
+                        self?.myTable.reloadData()
+                    }
+                } catch  {
+                    debugPrint(error)
+                }
+            }
+            else {
+                debugPrint("Something went wrong.")
+            }
         })
-         .resume()
+        .resume()
     }
 }
+
+
+
+
+
+
